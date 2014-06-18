a76dbf114f9902eff7e3c8a118e670732fb6c055
Christoph Bruecke <christoph.bruecke@campus.tu-berlin.de>
2014-06-18 14:33:47 +0200

    Initial version of Partial Order and CPO classes.

---
+++ Data/CPO.hs
@@ -0,0 +1,64 @@
+{-# LANGUAGE InstanceSigs #-}
+
+module Data.CPO where
+
+import Data.POrd
+import Data.Maybe
+import qualified Data.Set as S
+import qualified Data.Trie as T
+
+class (POrd a) => CPO a where
+    -- | Bottom element. Defaults to `undefined`.
+    --
+    -- prop> bottom `le` a
+    bottom :: a
+    bottom = undefined
+
+    -- | Least upper bound.
+    -- For 
+    --
+    -- Laws:
+    --
+    -- prop> a `le` (a `lub` b) && b `le` (a `lub` b)
+    -- prop> a `le` c && b `le` c && (a `lub` b) `le` c
+    --
+    -- Note: The default implementation @lub a b = if a <= b then b else a@
+    -- does not work since @a@ and @b@ might not be comparable. Hence returning
+    -- @b@ would violate the contract.
+    lub :: a -> a -> a
+
+instance CPO Int where
+    bottom = minBound
+    lub = max
+
+instance (CPO a) => CPO (Maybe a) where
+    bottom = Nothing
+
+    lub Nothing y         = y
+    lub x Nothing         = x
+    lub (Just x) (Just y) = Just (x `lub` y)
+
+instance (Ord a) => CPO (S.Set a) where
+    bottom = S.empty
+    lub = S.union
+
+instance (CPO b) =>CPO (T.Trie b) where
+    bottom = T.empty
+    lub m1 m2 = foldr merge T.empty dom'
+        where
+            -- list of unique values (from both domains)
+            dom' = S.toList $ T.dom m1 `S.union` T.dom m2
+            -- lookup for key -> value, return bottom if none found
+            lkup' k m = fromMaybe bottom $ T.lkup k m
+            merge k = T.insert k (lkup' k m1 `lub` lkup' k m2)
+
+instance (Eq a) => CPO [a] where
+    bottom = []
+
+    -- | This is a partial function. It is only defined if:
+    --
+    -- @l1 `le` l2 || l2 `le` l1@
+    lub l1 l2
+        | l1 `le` l2 = l2
+        | l2 `le` l1 = l1
+        | otherwise  = undefined
---
+++ Data/POrd.hs
@@ -0,0 +1,76 @@
+{-# LANGUAGE CPP #-}
+
+module Data.POrd where
+
+import Data.List
+import Data.Maybe
+import qualified Data.Set as S
+import qualified Data.Trie as T
+
+#if __GLASGOW_HASKELL__ < 708
+-- | Minimal complete definition: @le@.
+#endif
+class (Eq a) => POrd a where
+    compare :: a -> a -> Maybe Ordering
+    compare a b = case (le a b, le b a) of
+        (True,  True )  -> Just EQ
+        (True,  False)  -> Just LT
+        (False, True )  -> Just GT
+        _               -> Nothing
+
+    -- | Check whether @a <= b@.
+    --
+    -- prop> a `le` a == True
+    -- prop> a `le` b && b `le` a == (a == b)
+    -- prop> a `le` b && b `le` c == a `le` c
+    le :: a -> a -> Bool
+
+    eq :: a -> a -> Bool
+    x `eq` y = x `le` y && y `le` x
+
+    ge :: a -> a -> Bool
+    x `ge` y = y `le` x
+
+#if __GLASGOW_HASKELL__ >= 708
+    {-# MINIMAL le #-}
+#endif
+
+-- TODO make Ord a superclass of POrd (somehow?)
+
+instance POrd Int where
+    le = (<=)
+
+instance POrd Integer where
+    le = (<=)
+
+instance POrd Bool where
+    le = (<=)
+
+instance POrd Float where
+    le = (<=)
+
+instance POrd Double where
+    le = (<=)
+
+instance (POrd a) => POrd (Maybe a) where
+    le Nothing _       = True
+    le _       Nothing = False
+    le (Just a) (Just b) = a `le` b
+
+-- | Partial order over lists.
+--
+-- prop> (p1 ++ xs) `le` (p2 ++ ys) == (p1 == p2)
+instance (Eq a) => POrd [a] where
+    le = isPrefixOf
+
+-- | Partial order over sets.
+--
+-- prop> s1 `le` s2 == s1 isSubsetOf s2
+instance (Ord a) => POrd (S.Set a) where
+    le = S.isSubsetOf
+
+instance (POrd b) => POrd (T.Trie b) where
+    le m1 m2 = T.dom m1 `S.isSubsetOf` T.dom m2
+               && T.foldTK
+                  (\key val acc -> acc && val `le` fromJust (T.lkup key m2))
+                  True m2
