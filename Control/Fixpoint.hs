1c30fb7148c963f9c37f82589f6c6a4d51019bbf
Christoph Bruecke <christoph.bruecke@campus.tu-berlin.de>
2014-06-22 14:29:01 +0200

    Implement Fixpoint iteration.

---
+++ Control/Fixpoint.hs
@@ -0,0 +1,34 @@
+module Control.Fixpoint where
+
+import Data.POrd
+import Data.CPO
+import Data.Word
+import qualified Data.Set as S
+
+condFixpoint :: (CPO a) => (a -> a) -> a -> a
+condFixpoint f ws = if ws' == ws then ws else condFixpoint f ws'
+    where
+        ws' = f ws
+
+fixpoint :: (CPO a) => (a -> a) -> a
+fixpoint f = condFixpoint f bottom
+
+-- Test function copied from
+-- Pepper, Hofstedt "Funktionale Programmierung" p. 192
+
+-- | testFunction1 is continous function computing @1^n@.
+testFunction1 :: Word -> Word
+testFunction1 n = 1 ^ n
+
+testFunction3 :: Int -> Int
+testFunction3 x = x ^ (2 :: Int)
+
+testFunction4 :: S.Set Int -> S.Set Int
+testFunction4 = S.union (S.fromList [1, 7])
+
+instance POrd Word where
+    le = (<=)
+
+instance CPO Word where
+    bottom = minBound
+    lub = max
